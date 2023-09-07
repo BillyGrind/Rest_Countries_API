af3dcfdbbb94b6f2a30da64ff6f5b3f4eeefd11c
BillyGrind <denayeranthon1782@gmail.com>
2023-09-07 14:23:31 +0200

    wipWIPouille

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -3,13 +3,12 @@ import Navbar from "./Navbar";
 import {
   Image,
   Button,
-  Card,
   Text,
   Heading,
-  UnorderedList,
-  ListItem,
   Box,
   Flex,
+  Stack,
+  ButtonGroup,
 } from "@chakra-ui/react";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 
@@ -27,107 +26,99 @@ export default function CardDetail(props) {
     <>
       <Navbar />
       <Link to={`/`}>
-        <Button leftIcon={<ChevronLeftIcon />}>Back</Button>
+        <Button leftIcon={<ChevronLeftIcon />} m={6}>
+          Back
+        </Button>
       </Link>
-      <Card maxW="md">
+      <Stack m={6} spacing={6}>
         <Image
           src={country.state.flags.svg}
           alt={country.state.flags.alt}
           borderRadius="lg"
         />
-        <Heading size="lg">{country.state.name.common}</Heading>
-        <Flex>
-          <Text as="b">Native Name:&nbsp;</Text>
-          {country.state.name.nativeName ? (
-            country.state.name.nativeName[
-              Object.keys(country.state.name.nativeName)[0]
-            ].official
-          ) : (
-            <Text>No Native Name</Text>
-          )}
-        </Flex>
-
-        <Flex>
-          <Text as="b">Population :&nbsp;</Text>
-          <Text>{country.state.population}</Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Region:&nbsp;</Text>
-          <Text> {country.state.region}</Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Sub Region :&nbsp;</Text>
-          <Text> {country.state.subregion}</Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Capital :&nbsp;</Text>
-          <Text> {country.state.capital ? country.state.capital : "No"}</Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Top level domain :&nbsp;</Text>
-          <Text> {country.state.tld}</Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Currencies :&nbsp;</Text>
-          <Text>
-            {" "}
-            {country.state.currencies
-              ? country.state.currencies[
-                  Object.keys(country.state.currencies)[0]
-                ].name
-              : " No currencies"}
-          </Text>
-        </Flex>
-        <Flex>
-          <Text as="b">Languages :&nbsp;</Text>
+        <Box>
+          <Heading size="lg">{country.state.name.common}</Heading>
           <Flex>
-            {country.state.languages ? (
-              Object.values(country.state.languages).map((language, index) => (
-                <Text key={index}>&nbsp;{language} ,</Text>
-              ))
+            <Text as="b">Native Name:&nbsp;</Text>
+            {country.state.name.nativeName ? (
+              country.state.name.nativeName[
+                Object.keys(country.state.name.nativeName)[0]
+              ].official
             ) : (
-              <Text>No Languages</Text>
+              <Text>No Native Name</Text>
             )}
           </Flex>
-        </Flex>
 
-        <Flex direction={"row"}>
-          <Text as="b">Border Countries :&nbsp;</Text>
           <Flex>
+            <Text as="b">Population :&nbsp;</Text>
+            <Text>{country.state.population}</Text>
+          </Flex>
+          <Flex>
+            <Text as="b">Region:&nbsp;</Text>
+            <Text> {country.state.region}</Text>
+          </Flex>
+          <Flex>
+            <Text as="b">Sub Region :&nbsp;</Text>
+            <Text> {country.state.subregion}</Text>
+          </Flex>
+          <Flex>
+            <Text as="b">Capital :&nbsp;</Text>
+            <Text> {country.state.capital ? country.state.capital : "No"}</Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex>
+            <Text as="b">Top level domain :&nbsp;</Text>
+            <Text> {country.state.tld}</Text>
+          </Flex>
+          <Flex>
+            <Text as="b">Currencies :&nbsp;</Text>
+            <Text>
+              {" "}
+              {country.state.currencies
+                ? country.state.currencies[
+                    Object.keys(country.state.currencies)[0]
+                  ].name
+                : " No currencies"}
+            </Text>
+          </Flex>
+          <Flex>
+            <Text as="b">Languages :&nbsp;</Text>
+            <Flex>
+              {country.state.languages ? (
+                Object.values(country.state.languages).map(
+                  (language, index) => (
+                    <Text key={index}>&nbsp;{language} ,</Text>
+                  )
+                )
+              ) : (
+                <Text>No Languages</Text>
+              )}
+            </Flex>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Text as="b">Border Countries :&nbsp;</Text>
+          <Flex mt={2}>
             {country.state.borders ? (
-              <Box>
+              <ButtonGroup gap={2}>
                 {Object.values(country.state.borders).map((border, index) => (
                   <Link
                     to={`/country/${getCountry(border)}`}
                     key={index}
                     state={country}
                   >
-                    <Button key={index}>{border}</Button>
+                    <Button key={index} size={"sm"} variant="outline">{border}</Button>
                   </Link>
                 ))}
-              </Box>
+              </ButtonGroup>
             ) : (
               <Text>No borders countries</Text>
             )}
           </Flex>
-        </Flex>
-{/* 
-        {country.state.borders ? (
-          <UnorderedList>
-            {Object.values(country.state.borders).map((border, index) => (
-              <Link
-                to={`/country/${getCountry(border)}`}
-                key={index}
-                state={country}
-              >
-                <ListItem key={index}>{border}</ListItem>
-              </Link>
-            ))}
-          </UnorderedList>
-        ) : (
-          <Text>No borders countries</Text>
-        )} */}
-      </Card>
+        </Box>
+      </Stack>
     </>
   );
 }
